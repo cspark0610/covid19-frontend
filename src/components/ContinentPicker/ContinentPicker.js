be94887008542392f16b295a866f992836d0c8b1
cspark0610 <75094281+cspark0610@users.noreply.github.com>
2021-09-02 14:20:29 -0300

    Update ContinentPicker.js

--- src/components/ContinentPicker/ContinentPicker.js
+++ src/components/ContinentPicker/ContinentPicker.js
@@ -1,1 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl, Typography } from '@material-ui/core';
+import styles from '../CountryPicker/CountryPicker.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPostsByContinent } from '../../actions/posts';
+import Chart from '../Chart/Chart';
+
+const Continents = ({ data }) => {
+	const dispatch = useDispatch();
+	const showData = useSelector((state) => state.posts.posts);
+	const [selectedContinent, setSelectedContinent] = useState('');
+	const handleCountryChange = (e) => {
+		setSelectedContinent(e.target.value);
+	};
+	useEffect(() => {
+		console.log(selectedContinent);
+		dispatch(getPostsByContinent(selectedContinent));
+	}, [dispatch, selectedContinent]);
+
+	//console.log('showdata en continent', showData);
+
+	let totalRecovered = showData?.reduce((acc, item) => {
+		let obj = JSON.parse(item.cases);
+		return acc + obj.recovered;
+	}, 0);
+	let totalInfected = showData?.reduce((acc, item) => {
+		let obj = JSON.parse(item.cases);
+		return acc + obj.active;
+	}, 0);
+	let totalDead = showData?.reduce((acc, item) => {
+		let obj = JSON.parse(item.deaths);
+		return acc + obj.total;
+	}, 0);
+
+	return (
+		<>
+			<Typography gutterBottom variant="h4" component="h2">
+				Pick a Continent
+			</Typography>
+			<FormControl className={styles.formControl}>
+				<NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e)}>
+					<option value="">Select Continent</option>
+					{data &&
+						data.map((item, i) => (
+							<option key={i} value={item.continent}>
+								{item.continent}
+							</option>
+						))}
+				</NativeSelect>
+			</FormControl>
+
+			<br />
+			<br />
+			<Chart totalInfected={totalInfected} totalRecovered={totalRecovered} totalDead={totalDead} />
+		</>
+	);
+};
+
+export default Continents;
 
