8c8d2992648f3418e979e1aba0b0a5952d1942ab
cspark0610 <75094281+cspark0610@users.noreply.github.com>
2021-09-02 09:29:57 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,14 @@ import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
 
 const App = () => {
-	const user = JSON.parse(localStorage.getItem('profile'));
-
 	return (
 		<BrowserRouter>
 			<Container maxWidth="xl">
 				<Navbar />
 				<Switch>
-					<Route path="/" exact component={() => <Redirect to="/posts/statistics" />} />
-
+					<Route path="/" exact component={() => <Redirect to="/auth" />} />
 					<Route path="/posts/statistics" exact component={Home} />
 					<Route path="/auth" exact component={Auth} />
-
-					{/* <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts/statistics" />)} /> */}
 				</Switch>
 			</Container>
 		</BrowserRouter>
