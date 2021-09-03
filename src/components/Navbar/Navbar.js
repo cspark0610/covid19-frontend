7f9cb732fef02ac0a8decfb04a0d7a350d02b71b
cspark0610 <75094281+cspark0610@users.noreply.github.com>
2021-09-03 11:24:33 -0300

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,9 +13,7 @@ const Navbar = () => {
 
 	const logout = () => {
 		localStorage.clear();
-
 		history.push('/auth');
-
 		setUser(null);
 	};
 
@@ -32,27 +30,25 @@ const Navbar = () => {
 	return (
 		<AppBar className={classes.appBar} position="static" color="inherit">
 			<Toolbar className={classes.toolbar}>
-				{user?.result ? (
-					<div className={classes.profile}>
-						<Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>
-							{user?.result.name.charAt(0)}
-						</Avatar>
-						<Typography className={classes.userName} variant="h6">
-							{user?.result.name}
-						</Typography>
-						<Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>
-							Logout
-						</Button>
-					</div>
-				) : (
+				<div className={classes.profile}>
+					<Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>
+						{user?.result.name.charAt(0)}
+					</Avatar>
+					<Typography className={classes.userName} variant="h6">
+						{user?.result.name}
+					</Typography>
+					<Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>
+						Logout
+					</Button>
 					<Button component={Link} to="/auth" variant="contained" color="primary">
 						Login
 					</Button>
-				)}
+				</div>
 			</Toolbar>
 		</AppBar>
 	);
 };
 
 export default Navbar;
+
 
